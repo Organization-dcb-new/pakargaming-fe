5f0417cd190e1a01f6e820d5451e63a7468b1def
Muhammad Alfiandi Rizki <alfian.jw@gmail.com>
2026-02-26 16:16:52 +0700

    add try catch sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,19 +11,26 @@ const pages = [
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = process.env.NEXT_SITE_BASE_URL
-  const { data: games } = await serverApi.get<GetGamesResponse>('/v1/games')
 
-  const gameUrls =
-    games.data?.flatMap((game) => {
-      return routing.locales.map((locale) => ({
-        url: `${baseUrl}/${locale}/games/${game.slug}`,
-        lastModified: new Date(),
-        changeFrequency: 'daily' as const,
-        priority: 0.8,
-      }))
-    }) ?? []
+  let gameUrls: MetadataRoute.Sitemap = []
 
-  const staticUrls = routing.locales.flatMap((locale) =>
+  try {
+    const { data: games } = await serverApi.get<GetGamesResponse>('/v1/games')
+
+    gameUrls =
+      games.data?.flatMap((game) => {
+        return routing.locales.map((locale) => ({
+          url: `${baseUrl}/${locale}/games/${game.slug}`,
+          lastModified: new Date(),
+          changeFrequency: 'daily' as const,
+          priority: 0.8,
+        }))
+      }) ?? []
+  } catch (error) {
+    console.error('Sitemap Fetch Error:', error)
+  }
+
+  const staticUrls: MetadataRoute.Sitemap = routing.locales.flatMap((locale) =>
     pages.map((page) => ({
       url: `${baseUrl}/${locale}${page ? `/${page}` : ''}`,
       lastModified: new Date(),
